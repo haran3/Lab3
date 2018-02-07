de215af8a544e0f3cdd97467b7f966f28c473a76
Anand Haran <haran3@illinois.edu>
2018-02-06 20:35:14 -0600

    2nd commit.

--- src/webScraper.java
+++ src/webScraper.java
@@ -1,5 +1,35 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 public class webScraper {
-    public static void main(String[] unused) {
-        
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
     }
+    public static void main(String[] unused) {
+        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+    }
+
+    public static int wordNumber(String word) {
+        int count = 0;
+        for (int i = 0; i < urlToString("http://erdani.com/tdpl/hamlet.txt").length(); i++) {
+
+        }
+    }
+
+
 }
